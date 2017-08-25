f54603e826c964d4c8af910051e8b14bc805cd68
rdiez <rdiez@stratio.com>
2017-08-25 11:49:09 +0200

    Apply license

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -xe
+#Copyright (c) 2017. Stratio Big Data Inc., Sucursal en España. All rights reserved.
+
+#This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en
+#España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or
+#sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written
+#authorization from Stratio Big Data Inc., Sucursal en España.
 
 if [[ -z "$INFLUX_URL" ]]; then
 	FILE=jvm_file.json
